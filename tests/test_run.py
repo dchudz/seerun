98807f9c378d76e5d772b3e78447a4c17350e5ae
David Chudzicki <dchudz@gmail.com>
2018-03-26 07:43:26 -0400

    test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,13 +1,4 @@
-import logging
 from showvalues.run import run
-
-
-def test_baz(caplog):
-    logging.warn('hi')
-    for record in caplog.records:
-        assert record.levelname != 'CRITICAL'
-    print(caplog.text)
-    assert 'hi' in caplog.text
 
 
 def test_run_logs_exceptions(caplog):
@@ -18,6 +9,7 @@ def test_run_logs_exceptions(caplog):
 def test_run_logs_system_exit_1(caplog):
     run('raise SystemExit(1)', [], {})
     assert 'SystemExit' in caplog.text
+
 
 def test_run_no_log_system_exit_0(caplog):
     run('raise SystemExit(0)', [], {})
